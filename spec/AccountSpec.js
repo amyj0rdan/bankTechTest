5452c7b3396e3b0021a6b10f8884ff2525bbf9e5
Amy Jordan <amy_jordan@live.co.uk>
2019-05-07 12:11:09 +0100

    Client can make multiple deposits Deposits are stored in the transactions

--- spec/AccountSpec.js
+++ spec/AccountSpec.js
@@ -10,5 +10,11 @@ describe("Account", function() {
       account.deposit(1000, "10-01-2012")
       expect(account.getTransactions()).toEqual([[1000, "10-01-2012"]])
     });
+
+    it('can accept two deposits', function() {
+      account.deposit(1000, "10-01-2012")
+      account.deposit(2000, "13-01-2012")
+      expect(account.getTransactions()).toEqual([[1000, "10-01-2012"], [2000, "13-01-2012"]])
+    });
   });
 });
